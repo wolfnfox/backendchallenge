fcb63ba4ca03a661cfbf51d99b1a86c7deff41b4
Tom <thomasf@cleardata.co.uk>
2019-08-20 23:13:21 +0100

    Added Car model.

--- ottoweb/ottoapi/models.py
+++ ottoweb/ottoapi/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+WITH_OPTIONS = [('BR','Branch'),('DR','Driver')]
+
 # Create your models here.
+class Car(models.Model):
+    registration = models.CharField(unique=True,max_length=7)
+    make = models.CharField(max_length=32)
+    model = models.CharField(max_length=32)
+    year = models.DateField()
+    currently_with = models.CharField(choices=WITH_OPTIONS,default='Branch',max_length=2)
+    in_service = models.BooleanField(default=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['registration']
