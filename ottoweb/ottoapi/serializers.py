dcf9dbfb6f831bce2b8d3c4cf4473056632e1cc7
Tom <thomasf@cleardata.co.uk>
2019-08-21 00:24:03 +0100

    Added model serializers.

---
+++ ottoweb/ottoapi/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from ottoapi.models import Branch, Car, Driver
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = ['id','city','postcode']
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ['id','registration','make','model','year',\
+                  'currently_with','assigned_to','in_service','updated']
+
+class DriverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Driver
+        fields = ['id','name','date_of_birth']
